14e7fb2d9e154240c5a812a767e94f3065958da3
200506725@qq.com <gaojunchen520>
2019-07-08 17:50:07 +0800

    gateway-cloud

---
+++ gaoxiong-cloud-gateway/src/main/java/com/gaoxiong/gaoxiongcloudgateway/GaoxiongCloudGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.gaoxiong.gaoxiongcloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class GaoxiongCloudGatewayApplication {
+
+    public static void main ( String[] args ) {
+        SpringApplication.run(GaoxiongCloudGatewayApplication.class, args);
+    }
+
+}
